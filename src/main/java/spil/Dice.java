31eb4410697ec8517bfb19a1a0c27ee4801c290a
sebdtu <32480015+sebdtu@users.noreply.github.com>
2017-11-07 17:34:14 +0100

    Modificerer Dice så den passer til klassediagram

--- src/main/java/spil/Dice.java
+++ src/main/java/spil/Dice.java
@@ -2,13 +2,16 @@ package spil;
 
 public class Dice {
     private int faceValue;
+    private int numberOfEyes;
 
+    public Dice(int numberOfEyes){this.numberOfEyes = numberOfEyes;}
 
     public void roll()
     {
-        this.faceValue = (int) (Math.random() * 6) + 1;
+        this.faceValue = (int) (Math.random() * numberOfEyes) + 1;
     }
 
+    public int getNumberOfEyes(){return this.numberOfEyes;}
 
     public int getFaceValue()
     {
