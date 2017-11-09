fd19175e80b6613036b521d20c3703cf8ec9f2ea
sebdtu <32480015+sebdtu@users.noreply.github.com>
2017-11-09 17:27:34 +0100

    Junit Player

---
+++ src/test/java/spil/PlayerTest.java
@@ -0,0 +1,39 @@
+package spil;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getName() throws Exception {
+        Player player = new Player("test");
+        assertEquals("test",player.getName());
+
+    }
+
+    @Test
+    public void getAmount() throws Exception {
+        Player player = new Player("test");
+        assertEquals(1000, player.getAmount());
+    }
+
+    @Test
+    public void addAmount() throws Exception {
+        Player player = new Player("test");
+        //Vi sætter vores forventede amount til 1200, og prøver så at bruge addAmount(200)
+        player.addAmount(200);
+        assertEquals(1200,player.getAmount());
+    }
+
+}
