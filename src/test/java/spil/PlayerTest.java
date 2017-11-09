6dc71bcf08df7bdf7021123b9795523acdf19f5a
sebdtu <32480015+sebdtu@users.noreply.github.com>
2017-11-09 17:51:47 +0100

    Junit addAmount negativ test

--- src/test/java/spil/PlayerTest.java
+++ src/test/java/spil/PlayerTest.java
@@ -31,10 +31,14 @@ public class PlayerTest {
     @Test
     public void addAmount() throws Exception {
         Player player = new Player("test");
+        //Vi starter med at være sikre på at amount = 1000. Dette gør vi for at se om vores addAmount ændrer på noget.
         assertEquals(1000,player.getAmount());
-        //Vi sætter vores forventede amount til 1200, og prøver så at bruge addAmount(200)
+        //Vi sætter vores forventede amount til 1200, og prøver så at bruge addAmount(200).
         player.addAmount(200);
         assertEquals(1200,player.getAmount());
+        //Vi prøver at bruge addAmount til at få amount<0, og dermed sikre at den sætter amount=0 når amount<0.
+        player.addAmount(-1300);
+        assertEquals(0,player.getAmount());
     }
 
 }
--- src/test/java/spil/StashTest.java
+++ src/test/java/spil/StashTest.java
@@ -24,10 +24,14 @@ public class StashTest {
     @Test
     public void addAmount() throws Exception {
         Stash stash = new Stash();
+        //Vi starter med at være sikre på at amount = 1000. Dette gør vi for at se om vores addAmount ændrer på noget.
         assertEquals(1000,stash.getAmount());
         //Vi sætter vores forventede amount til 1400, og prøver så at bruge addAmount(400)
         stash.addAmount(400);
         assertEquals(1400,stash.getAmount());
+        //Vi prøver at bruge addAmount til at få amount<0, og dermed sikre at den sætter amount=0 når amount<0.
+        stash.addAmount(-1600);
+        assertEquals(0,stash.getAmount());
     }
 
 }
