cc0cd92060b2aa6245ffdee496b3815cf9d8a416
sebdtu <32480015+sebdtu@users.noreply.github.com>
2017-11-09 17:08:14 +0100

    Junit Stash

---
+++ src/test/java/spil/StashTest.java
@@ -0,0 +1,32 @@
+package spil;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StashTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getAmount() throws Exception {
+        Stash stash = new Stash();
+        assertEquals(1000,stash.getAmount());
+    }
+
+    @Test
+    public void addAmount() throws Exception {
+        Stash stash = new Stash();
+        //Vi sætter vores forventede amount til 1400, og prøver så at bruge addAmount(400)
+        stash.addAmount(400);
+        assertEquals(1400,stash.getAmount());
+    }
+
+}
