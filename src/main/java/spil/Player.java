677e13801386c4231433f0f1757a5e46e5126156
sebdtu <32480015+sebdtu@users.noreply.github.com>
2017-11-10 10:52:49 +0100

    Kommentarer player

--- src/main/java/spil/Player.java
+++ src/main/java/spil/Player.java
@@ -5,6 +5,7 @@ public class Player {
     private String name;
     private Stash stash;
 
+    //Creates a player, with the name that the user types in. It also creates a Stash with the startamount = 1000
     public Player(String name)
     {
         this.name = name;
@@ -21,6 +22,7 @@ public class Player {
         return this.stash.getAmount();
     }
 
+    //This method adds an amount to the players stash.
     public void addAmount(int numberToAdd)
     {
         this.stash.addAmount(numberToAdd);
