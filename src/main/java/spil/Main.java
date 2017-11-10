61887c112d6ab1c4d48565dd3f424fbaa330d97c
Oliver <32456406+outrageousbog@users.noreply.github.com>
2017-11-10 15:06:08 +0100

    Added comments to main-class

--- src/main/java/spil/Main.java
+++ src/main/java/spil/Main.java
@@ -3,8 +3,11 @@ package spil;
 
 public class Main {
     public static void main(String[] args) {
+        //The game-object is created
         Game game = new Game();
+        //The askForPlayer() can be changed if more players are needed to play the game
         game.askForPlayers(2);
+        //the Game-class contains the game, and the the method play() plays it
         game.play();
     }
 }
