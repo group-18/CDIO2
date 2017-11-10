fe33f9a559d9681759d289d8a0a092ebea3a28c6
sebdtu <32480015+sebdtu@users.noreply.github.com>
2017-11-10 10:54:49 +0100

    Changed Junit for stash to fit the startvalue of game

--- src/test/java/spil/StashTest.java
+++ src/test/java/spil/StashTest.java
@@ -11,21 +11,21 @@ public class StashTest {
 
     @Before
     public void setUp() {
-        this.stash = new Stash();
+        this.stash = new Stash(1000);
     }
 
     @Test
     public void getAmount() throws Exception {
-        assertEquals(0, this.stash.getAmount());
+        assertEquals(1000, this.stash.getAmount());
     }
 
     @Test
     public void addAmount() throws Exception {
         //Vi starter med at være sikre på at amount = 0. Dette gør vi for at se om vores addAmount ændrer på noget.
-        assertEquals(0, this.stash.getAmount());
+        assertEquals(1000, this.stash.getAmount());
         //Vi prøver så at bruge addAmount(400), for at se om den får tilføjet værdien.
         stash.addAmount(400);
-        assertEquals(400, this.stash.getAmount());
+        assertEquals(1400, this.stash.getAmount());
         //Vi prøver at bruge addAmount til at få amount<0, og dermed sikre at den sætter amount=0 når amount<0.
         stash.addAmount(-1600);
         assertEquals(0, this.stash.getAmount());
